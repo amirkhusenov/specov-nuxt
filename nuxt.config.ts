844552f09e7906f478854aa83fa02a1ab144efd5
Amiri <husenova270@gmail.com>
2025-12-17 13:19:52 +0400

    fix fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", '@pinia/nuxt'],
   css: ["~/assets/css/main.css"],
-  ui: {
-    fonts: false
-  }
+  // ui: {
+  //   fonts: false
+  // }
 });
 
